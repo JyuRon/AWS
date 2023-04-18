82d12e6f60e6e28cdc4509f122580deba6781587
Jyuka <yoyksks3@gmail.com>
2023-04-18 21:38:21 +0900

    deploy.yaml & application.yaml 연동 테스트

--- src/main/java/com/example/controller/DemoController.java
+++ src/main/java/com/example/controller/DemoController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class DemoController {
 
+    @Value("${override.value}")
+    private String value;
+
     @GetMapping("/name")
     public String name(){
-        return "jyuka";
+        return value+" jyuka";
     }
 
 }
